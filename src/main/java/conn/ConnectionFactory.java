e0dd4677560eef125919442304a30ee1cf1d60ef
ze-fernando <jfenginner935@gmail.com>
2023-08-20 14:39:35 -0300

    Created class

---
+++ src/main/java/conn/ConnectionFactory.java
@@ -0,0 +1,15 @@
+package conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/animeStore";
+        String username = "root";
+        String password = "root";
+
+        return DriverManager.getConnection(url, username, password);
+    }
+}
---
+++ src/main/java/repository/ProducerRepository.java
@@ -0,0 +1,44 @@
+package repository;
+
+import conn.ConnectionFactory;
+import domain.Producer;
+import lombok.extern.log4j.Log4j2;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Log4j2
+public class ProducerRepository {
+    public static List<Producer> findByNamePreparedStatement(String name){
+        List<Producer> producers = new ArrayList<>();
+        try (Connection con = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPreparedStatement(con, name);
+             ResultSet rs = ps.executeQuery()){
+
+            while(rs.next()){
+                Producer producer = Producer.builder()
+                        .name(rs.getString("name"))
+                        .id(rs.getInt("id"))
+                        .build();
+                producers.add(producer);
+            }
+        } catch (SQLException e){
+            log.error("Error while trying to find producer", e);
+        }
+        return producers;
+    }
+
+    private static PreparedStatement createPreparedStatement(Connection con, String name)
+            throws SQLException {
+        String sql = "SELECT * FROM animeStore.producer WHERE name like ?;";
+
+        var ps = con.prepareStatement(sql);
+        ps.setString(1, name);
+        return ps;
+    }
+}
